60f354a8ee0ca4413c5c0fe147a9d580fed27a9f
Leonardo Gimenes <lgimenes07@gmail.com>
2021-01-13 18:38:13 -0300

    Refactor to reverse proxy

--- src/main/java/com/starwars/feigndemo/api/StarWarsApiClient.java
+++ src/main/java/com/starwars/feigndemo/api/StarWarsApiClient.java
@@ -1,8 +1,10 @@
 package com.starwars.feigndemo.api;
 
 import com.starwars.feigndemo.dto.StarWarsApiCharacterResponse;
+import com.starwars.feigndemo.dto.StarWarsCharacter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "starwarsapi", url = "${api.starwars}")
@@ -10,4 +12,7 @@ public interface StarWarsApiClient {
 
     @GetMapping("/people/")
     StarWarsApiCharacterResponse searchForCharacters(@RequestParam("page") final Integer page);
+
+    @GetMapping("/people/{id}/")
+    StarWarsCharacter searchForCharacter(@PathVariable("id") final Integer id);
 }
---
+++ src/main/java/com/starwars/feigndemo/service/CharactersController.java
@@ -0,0 +1,30 @@
+package com.starwars.feigndemo.service;
+
+import com.starwars.feigndemo.api.StarWarsApiClient;
+import com.starwars.feigndemo.dto.StarWarsApiCharacterResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class CharactersController {
+
+    private final StarWarsApiClient starWarsApiClient;
+
+    public CharactersController(StarWarsApiClient starWarsApiClient) {
+        this.starWarsApiClient = starWarsApiClient;
+    }
+
+    @GetMapping(value = "/people/")
+    public StarWarsApiCharacterResponse searchCharacters(@RequestParam(value = "page", required = false) final Integer page){
+        return starWarsApiClient.searchForCharacters(page);
+    }
+
+    @GetMapping(value = "/people/{id}/")
+    public StarWarsApiCharacterResponse searchCharacterById(@PathVariable("id") Integer id) {
+        return starWarsApiClient.searchForCharacters(id);
+    }
+}
